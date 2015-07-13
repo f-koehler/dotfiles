7baed83764f5c45c7783a4145a6a3f8aed20a9e6
f-koehler <fabian2804@googlemail.com>
2015-07-13 23:48:02 +0200

    some offlineimap changes. need to update muttrc to finally use mutt in a productive fashion

--- .offlineimap.py
+++ .offlineimap.py
@@ -1,5 +1,9 @@
-import subprocess
+#!/bin/python
+from subprocess import check_output
 
 def mailpasswd(account):
-    path = "/home/fabian/.mail-%s-passwd.gpg" % account
-    return subprocess.check_output(["gpg", "--quiet", "--batch", "-d", path]).strip()
+    return check_output("pass mail/"+account, shell=True).rstrip()
+
+# def mailpasswd(account):
+#     path = "/home/fabian/.mail-%s-passwd.gpg" % account
+#     return subprocess.check_output(["gpg", "--quiet", "--batch", "-d", path]).strip()
