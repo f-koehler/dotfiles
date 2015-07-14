d90818217e8230c4e810397c04cbf77fb50b796b
f-koehler <fabian2804@googlemail.com>
2015-07-14 20:24:45 +0200

    msmtp: use pass as well

--- .offlineimap.py
+++ .offlineimap.py
@@ -3,7 +3,3 @@ from subprocess import check_output
 
 def mailpasswd(account):
     return check_output("pass mail/"+account, shell=True).rstrip()
-
-# def mailpasswd(account):
-#     path = "/home/fabian/.mail-%s-passwd.gpg" % account
-#     return subprocess.check_output(["gpg", "--quiet", "--batch", "-d", path]).strip()
