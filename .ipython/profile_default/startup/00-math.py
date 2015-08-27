625dbb9d3349719504d928aa180ff50bb6e5621e
Fabian Köhler <fkoehler1024@googlemail.com>
2015-08-27 19:42:36 +0200

    cleanup

--- .ipython/profile_default/startup/00-math.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from scipy import stats,optimize
-from matplotlib import pyplot as plt
-import sympy as sym
-from uncertainties import *
--- .offlineimap.py
+++
@@ -1,5 +0,0 @@
-#!/bin/python
-from subprocess import check_output
-
-def mailpasswd(account):
-    return check_output("pass mail/"+account, shell=True).rstrip()
