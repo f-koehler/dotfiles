7098a1a0ce44e6f514e821ef05e39d2d09cba470
Fabian Köhler <fkoehler1024@googlemail.com>
2015-10-12 21:44:59 +0200

    add Lmod

--- .local.zsh
+++ .local.zsh
@@ -10,3 +10,5 @@ ZSH_THEME_GIT_PROMPT_SUFFIX=") "
 export PATH=/usr/local/texlive/2015/bin/x86_64-linux:$PATH
 export INFOPATH=/usr/local/texlive/2015/texmf-dist/doc/info:$INFOPATH
 export MANPATH=/usr/local/texlive/2015/texmf-dist/doc/man:$MANPATH
+
+export PATH=/usr/local/lmod/lmod/libexec:$PATH
