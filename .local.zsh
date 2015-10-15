8d7c1507653001a1aaeefaf29214f97ac7a2e26a
Fabian Köhler <fkoehler1024@googlemail.com>
2015-10-15 18:38:51 +0200

    zsh: yellow line

--- .local.zsh
+++ .local.zsh
@@ -1,7 +1,7 @@
 bindkey "^[Od" backward-word
 bindkey "^[Oc" forward-word
 
-PS1_LINE=$'%{$fg[blue]%}${(r:$(expr $COLUMNS - 9)::\u2500:)} %*\n'
+PS1_LINE=$'%{$fg[yellow]%}${(r:$(expr $COLUMNS - 9)::\u2500:)} %*\n'
 PS1_PROMPT=$'%{$fg[green]%}%B%n@%m%b %{$fg[red]%}%~ %{$fg[green]%}$(git_prompt_info)%B%{$fg[green]%}\$%b %{$reset_color%}'
 export PS1=${PS1_LINE}${PS1_PROMPT}
 ZSH_THEME_GIT_PROMPT_PREFIX="("
