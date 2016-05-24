237bd1fd1a7a90636c9105529ebfe4be604ab2c0
Fabian Köhler <fkoehler1024@googlemail.com>
2016-05-24 11:58:47 +0200

    add auto_cd command to zshrc, change TeXLive paths

--- .local.sh
+++ .local.sh
@@ -10,9 +10,9 @@ alias ll="ls -lh"
 export PATH=${HOME}/.local/bin:${PATH}
 
 # add TeX Live paths
-export PATH=/usr/local/texlive/2015/bin/x86_64-linux:${PATH}
-export INFOPATH=/usr/local/texlive/2015/texmf-dist/doc/info:${INFOPATH}
-export MANPATH=/usr/local/texlive/2015/texmf-dist/doc/man:${MANPATH}
+export PATH=/usr/local/texlive/2016/bin/x86_64-linux:${PATH}
+export INFOPATH=/usr/local/texlive/2016/texmf-dist/doc/info:${INFOPATH}
+export MANPATH=/usr/local/texlive/2016/texmf-dist/doc/man:${MANPATH}
 
 # add Lmod paths
 export PATH=/usr/local/lmod/lmod/libexec:${PATH}
--- .zshrc
+++ .zshrc
@@ -34,6 +34,8 @@ ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=10'
 bindkey "^[Od" backward-word
 bindkey "^[Oc" forward-word
 
+setopt auto_cd
+
 # custom prompt
 PS1_LINE=$'%{$fg[yellow]%}${(r:$(expr $COLUMNS - 9)::\u2500:)} %*\n'
 PS1_PROMPT=$'%{$fg[green]%}%B%n@%m%b %{$fg[red]%}%~ %{$fg[green]%}$(git_prompt_info)%B%{$fg[green]%}\$%b %{$reset_color%}'
