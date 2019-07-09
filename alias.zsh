4afe06cf6a54ef2e4e5952e36e57aa7f23ff72d0
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2019-07-09 13:43:32 +0200

    Add aliases for exa.

--- alias.zsh
+++ alias.zsh
@@ -1,1 +1,7 @@
-alias ll="ls -lh --color=always"
+if (( $+commands[exa] )); then
+    alias ll="exa -lh"
+    alias ls="exa"
+else
+    alias ll="ls -lh --color=always"
+    alias ls="ls --color=always"
+fi
