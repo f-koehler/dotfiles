29021919dfa0ef2762feb5f5c5bac6d298001635
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2019-07-17 13:29:28 +0200

    Add conda config.

--- init.zsh
+++ init.zsh
@@ -74,3 +74,19 @@ export QT_API="pyside2"
 
 source $HOME/.zsh.d/android.zsh
 source $HOME/.zsh.d/cocos2d-x.zsh
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/fkoehler/.local/opt/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/fkoehler/.local/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/fkoehler/.local/opt/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/fkoehler/.local/opt/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
