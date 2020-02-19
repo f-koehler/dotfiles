b2698c10f40be5f64de7b2cb1376245bead6a4a7
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2020-02-19 14:25:38 +0100

    Remove old environments.

--- init.zsh
+++ init.zsh
@@ -75,34 +75,13 @@ export PATH=$HOME/.local/bin:$PATH
 
 source /opt/spack/share/spack/setup-env.sh
 
-source /opt/intel/compilers_and_libraries_2019.4.243/linux/bin/compilervars.sh intel64
+source /opt/intel/compilers_and_libraries_2020.0.166/linux/bin/compilervars.sh intel64
 
 export MODULEPATH="$HOME/.local/share/modulefiles:$MODULEPATH"
 
 
-if [ -f /etc/profile.d/modules.sh ]; then
-    source /etc/profile.d/modules.sh
-    if [ -f $HOME/.local/share/modulefiles/anaconda/3 ]; then
-        module load anaconda/3
-    fi
-fi
 export QT_API="pyside2"
 
-source $HOME/.zsh.d/android.zsh
-source $HOME/.zsh.d/cocos2d-x.zsh
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/fkoehler/.local/opt/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/home/fkoehler/.local/opt/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/fkoehler/.local/opt/anaconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/home/fkoehler/.local/opt/anaconda3/bin:$PATH"
-    fi
+if [[ -n $VIRTUAL_ENV && -e "${VIRTUAL_ENV}/bin/activate" ]]; then
+# source "${VIRTUAL_ENV}/bin/activate"  # commented out by conda initialize
 fi
-unset __conda_setup
-# <<< conda initialize <<<
-
