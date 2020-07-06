f68d086658683f90ddcd5080e83ed81ab1aba449
Fabian Köhler <fkoehler@physnet.uni-hamburg.de>
2020-07-06 11:18:06 +0200

    Set fzf colors.

--- init.zsh
+++ init.zsh
@@ -69,6 +69,12 @@ source "$ZSHD/functions.zsh"
 # Load my fzf tweaks
 source "$ZSHD/fzf.zsh"
 
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+--color=dark
+--color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+--color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
+'
+
 # Set editor to vim
 export EDITOR=vim
 
