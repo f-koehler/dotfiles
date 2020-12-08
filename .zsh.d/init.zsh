5e3d03f77fb5bf8517aacfc98183bae96520687f
Fabian Köhler <fabian.koehler@protonmail.ch>
2020-12-08 10:00:56 +0100

    Update submodules.

--- .zsh.d/init.zsh
+++ .zsh.d/init.zsh
@@ -112,3 +112,18 @@ fi
 if [ -d "$HOME/.local/share/flatpak/exports/bin/" ]; then
   export PATH="$HOME/.local/share/flatpak/exports/bin/:$PATH"
 fi
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/fkoehler/.local/opt/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/fkoehler/.local/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/fkoehler/.local/opt/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/fkoehler/.local/opt/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
