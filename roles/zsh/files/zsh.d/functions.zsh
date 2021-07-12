46068dcc84087f3e2e012de65a8f6c30653a90d6
Fabian Köhler <fabian.koehler@protonmail.ch>
2021-07-12 11:48:54 +0200

    Remove now obolete clones from zsh plugin management.

--- roles/zsh/files/zsh.d/functions.zsh
+++ roles/zsh/files/zsh.d/functions.zsh
@@ -14,8 +14,6 @@ function zsh_add_plugin() {
         zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
         zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh" || \
         zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh-theme"
-    else
-        git clone "https://github.com/$1.git" "$ZSHD/plugins/$PLUGIN_NAME"
     fi
 }
 
@@ -26,10 +24,6 @@ function zsh_add_completion() {
 		completion_file_path=$(ls $ZSHD/plugins/$PLUGIN_NAME/_*)
 		fpath+="$(dirname "${completion_file_path}")"
         zsh_add_file "plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh"
-    else
-        git clone "https://github.com/$1.git" "$ZSHD/plugins/$PLUGIN_NAME"
-		fpath+=$(ls $ZSHD/plugins/$PLUGIN_NAME/_*)
-        [ -f $ZSHD/.zccompdump ] && $ZSHD/.zccompdump
     fi
 	completion_file="$(basename "${completion_file_path}")"
 	if [ "$2" = true ] && compinit "${completion_file:1}"
