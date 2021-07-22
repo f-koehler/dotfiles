584d5ed17b7977392bf38ebe3a72c0ce848423a3
Fabian Köhler <fabian.koehler@protonmail.ch>
2021-07-22 10:53:34 +0200

    Fix nvim role.

--- roles/nvim/files/lv-config.lua
+++ roles/nvim/files/lv-config.lua
@@ -1,25 +1,24 @@
-O.auto_complete = true
-O.default_options.relativenumber = false
+O.format_on_save = true
+O.lint_on_save = true
+O.completion.autocomplete = true
 O.colorscheme = "spacegray"
+O.default_options.wrap = true
 O.default_options.timeoutlen = 100
-O.leader_key = " "
-
+O.default_options.relativenumber = false
+O.keys.leader_key = "space"
 
 O.plugin.dashboard.active = true
-O.plugin.zen.active = true
+O.plugin.terminal.active = true
+O.plugin.zen.active = false
+O.plugin.zen.window.height = 0.90
+O.plugin.nvimtree.side = "left"
 
 O.treesitter.ensure_installed = "all"
 O.treesitter.highlight.enabled = true
 
--- lua
-O.lang.lua.autoformat = true
-O.lang.lua.formatter = "lua-format"
-
-
 -- python
-O.lang.python.autoformat = true
 O.lang.python.isort = true
-O.lang.python.linter = "flake8"
+O.lang.python.linters = { "flake8", "mypy" }
 O.lang.python.diagnostics.virtual_text = true
 O.lang.python.analysis.use_library_code_types = true
 O.lang.python.formatter.exe = "black"
