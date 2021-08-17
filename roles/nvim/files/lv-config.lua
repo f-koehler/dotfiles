fac2f99498368b159faa0176235c540f0495dc38
Fabian Köhler <fabian.koehler@protonmail.ch>
2021-08-17 11:43:20 +0200

    Update lv-config.

--- roles/nvim/files/lv-config.lua
+++ roles/nvim/files/lv-config.lua
@@ -3,17 +3,27 @@ lvim.lint_on_save = true
 lvim.colorscheme = "spacegray"
 lvim.leader = "space"
 
-lvim.builtin.compe.autocomplete = true
 lvim.builtin.dashboard.active = true
 lvim.builtin.terminal.active = true
 
-lvim.builtin.treesitter.ensure_installed = "all"
+lvim.builtin.treesitter.ensure_installed = "maintained"
 lvim.builtin.treesitter.highlight_enabled = true
-
-lvim.lang.python.formatter.exe = "black"
 
 lvim.lsp.diagnostics.virtual_text = false
 
 lvim.plugins = {
     {"dstein64/vim-startuptime"},
+}
+
+lvim.lang.python.formatters = {
+  {
+    exe = "black",
+    args = {}
+  }
+}
+lvim.lang.python.linters = {
+  {
+    exe = "flake8",
+    args = {}
+  }
 }
