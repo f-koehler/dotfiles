bd2a1a2a0e4bc04e553cae754eb334b63a68fef5
Fabian Köhler <fabian.koehler@protonmail.ch>
2022-01-20 13:44:58 +0100

    Use default fzf colors.

--- roles/zsh/files/zsh/fzf.zsh
+++ roles/zsh/files/zsh/fzf.zsh
@@ -16,11 +16,11 @@ elif [[ -f /usr/share/doc/fzf/examples/completion.zsh ]]; then
   source /usr/share/doc/fzf/examples/completion.zsh
 fi
 
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
---color=dark
---color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
---color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
-'
+# export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
+# --color=dark
+# --color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+# --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
+# '
 
 
 # export FZF_DEFAULT_COMMAND='
