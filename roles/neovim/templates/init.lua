8d32d3a7cf828c3e55f6fd00c5a455638a90e60e
Fabian Köhler <fabian.koehler@protonmail.ch>
2022-12-13 15:36:06 +0100

    Add neovim role.

---
+++ roles/neovim/templates/init.lua
@@ -0,0 +1,6 @@
+if vim.g.vscode then
+    vim.keymap.set("x", "gc", "<Plug>VSCodeCommentary")
+    vim.keymap.set("n", "gc", "<Plug>VSCodeCommentary")
+    vim.keymap.set("o", "gc", "<Plug>VSCodeCommentary")
+    vim.keymap.set("n", "gcc", "<Plug>VSCodeCommentaryLine")
+end
