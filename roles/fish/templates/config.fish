c43e3e9fd87cea2f4e8912abb68912c3c9de2914
Fabian Köhler <fabian.koehler@protonmail.ch>
2023-05-23 12:24:02 +0200

    Store shell hooks in separate files.

--- roles/fish/templates/config.fish
+++ roles/fish/templates/config.fish
@@ -1,11 +1,11 @@
+{% for alias in aliases %}
+alias {{ alias[0] }}='{{ alias[1] }}'
+{% endfor %}
+
 {% for hook in fish_hooks %}
 {{ hook }}
 {% endfor %}
 
 {% for export in exports %}
 set -U {{ export|upper }} {{ exports[export] }}
-{% endfor %}
-
-{% for alias in aliases %}
-alias {{ alias[0] }}='{{ alias[1] }}'
 {% endfor %}
